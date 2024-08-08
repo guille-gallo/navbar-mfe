59643792baee7608d5afff88a2d0adb049e155af
guille-gallo <guille.gallo88@gmail.com>
2024-08-08 18:06:18 +0200

    delete file

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar/sidebar.component";
-import './root.component.scss';
 
 export default function Root() {
   return (
